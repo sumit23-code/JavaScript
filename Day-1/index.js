e7c5a9b236d1b0146fe942f20c9095006c38c512
sumit23-code <sumitfulsundar732@gmail.com>
2025-06-28 13:33:03 +0530

    Update for Day-1

--- Day-1/index.js
+++ Day-1/index.js
@@ -1,21 +1,21 @@
-// var number1 = 5;
-// var number2 = 15;
-// var result = number1 + number2
-// console.log(result);
+var number1 = 5;
+var number2 = 15;
+var result = number1 + number2
+console.log(result);
 
 
 
 
 
-// var counter = 2;
-// counter = counter -3;
-// console.log(counter)
+var counter = 2;
+counter = counter -3;
+console.log(counter)
 
 
 
 
-// var number;
-// console.log(number, typeof number);
+var number;
+console.log(number, typeof number);
 
 
 
