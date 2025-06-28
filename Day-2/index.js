b2140b262aa4a024a5eeef51caeedc1fab627beb
sumit23-code <sumitfulsundar732@gmail.com>
2025-06-28 13:33:21 +0530

    Update for Day-2

--- Day-2/index.js
+++ Day-2/index.js
@@ -1,25 +1,25 @@
-// console.log(5==5)
-// console.log(10==5)
-// console.log("100"==100)
-// console.log(100=="100")
-// console.log(50==="50")
+console.log(5==5)
+console.log(10==5)
+console.log("100"==100)
+console.log(100=="100")
+console.log(50==="50")
 
 
 
-// console.log(100>50)
-// console.log(100>10)
-// console.log(25<10)
+console.log(100>50)
+console.log(100>10)
+console.log(25<10)
 
 
-// console.log(5>=10)
-// console.log(5>=4)
-// console.log(10<=9)
+console.log(5>=10)
+console.log(5>=4)
+console.log(10<=9)
 
 
 
-// console.log(8>5 && 6>2)
-// console.log(10>8 && 25<60 && 85<90)
-// console.log(90>45 && 45<72 && 12>15)
+console.log(8>5 && 6>2)
+console.log(10>8 && 25<60 && 85<90)
+console.log(90>45 && 45<72 && 12>15)
 
 
 
